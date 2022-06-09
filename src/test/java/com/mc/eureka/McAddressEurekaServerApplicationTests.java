1bae8f4619b5e46c01a200c091f4ea844e131a20
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-06-09 13:47:26 +0000

    First commit.

---
+++ src/test/java/com/mc/eureka/McAddressEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McAddressEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
